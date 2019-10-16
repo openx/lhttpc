955c098595bf06b850ec7c15926439d6dc0007f4
David Hull <david.hull@openx.com>
2019-10-16 00:14:35 +0000

    Do dynamic DNS lookups in a separate process.  Version 1.15.0.
    Now when lhttpc_dns detects that DNS resolution is changing dynamically it
    does a batch of DNS lookups in a separate process.  This should allow it to
    more reliably get and keep the full set of IP addresses for a domain.

--- src/lhttpc_dns.erl
+++ src/lhttpc_dns.erl
@@ -8,6 +8,7 @@
         , lookup/1
         , lookup_uncached/1
         , monotonic_time/0
+        , expire_dynamic/1
         , print_cache/0
         , print_cache/1
         , print_tcpdump_hosts/1
@@ -21,20 +22,30 @@
 -type ttl() :: pos_integer().
 
 -type ip_entry () :: {inet:ip_address(), native_time() | 'infinity'}.
-%% Tuple of any length is OK, but 2 is enough to satisfy dialyzer.
+%% Tuple of any length greater than 1 is OK, but 2 is enough to satisfy dialyzer.
 -type ip_addrs () :: {ip_entry()} | {ip_entry(), ip_entry()}.
 
--type dns_stats() :: { SuccessCount :: non_neg_integer()
-                     , FailureCount :: non_neg_integer()
-                     , FailuresSinceLastSuccessCount :: non_neg_integer()
-                     , SuccessTime :: integer() | 'undefined' }.
+-record(dns_stats,
+        { success_count = 0 :: non_neg_integer()
+        , failure_count = 0 :: non_neg_integer()
+        , failures_since_last_success_count = 0 :: non_neg_integer()
+        , success_time :: native_time() | 'undefined'
+        }).
+
+-type dns_stats() :: #dns_stats{}.
 
 -record(dns_rec,
         { hostname :: inet:hostname()
         , ip_addrs :: ip_addrs()            % list_to_tuple(list(ip_entry())).
         , expiration_time :: native_time()
-        , stats :: dns_stats()
+        , dynamic_count = 0 :: non_neg_integer()
+        , dynamic_pid :: pid() | 'undefined'
+        , stats = #dns_stats{} :: dns_stats()
         }).
+
+-define(DNS_DYNAMIC, 0).
+-define(DNS_STATIC, 1).
+-type dns_type() :: ?DNS_DYNAMIC | ?DNS_STATIC.
 
 
 init (_Args) ->
@@ -62,6 +73,7 @@ monotonic_time() ->
 -define(MAX_CACHE_SECONDS, 3600).
 -define(MIN_CACHE_SECONDS, 30).
 -define(DYNAMIC_REQUERY_SECONDS, 4).
+-define(DYNAMIC_REQUERY_COUNT, 12).
 -define(MAX_CACHE_SIZE, 10000).
 
 
@@ -91,23 +103,27 @@ lookup (Host) ->
       choose_addr(CachedIPAddrs);
     true ->
       %% Cached lookup failed.  Perform lookup and cache results.
-      {{IPAddrs, RefreshTS}, Stats} =
-        case lhttpc_dns:lookup_uncached(Host) of
-          {undefined, FailureTTL} ->
-            %% If the lookup fails but we have a stale cached result, keep it.
-            UpdatedFailureLookup =
-              case CachedIPAddrs of
-                undefined -> {undefined, ?TTL_TO_TS(FailureTTL, Now)};
-                _         -> {CachedIPAddrs, ?TTL_TO_TS(?MIN_CACHE_SECONDS, Now)}
-              end,
-            {UpdatedFailureLookup, count_failure(CachedStats)};
-          SuccessLookup ->
-            MergedLookup = merge_addrs(SuccessLookup, CachedIPAddrs, Now),
-            {MergedLookup, count_success(CachedStats, Now)}
-        end,
+      case lhttpc_dns:lookup_uncached(Host) of
+        {undefined, FailureTTL} ->
+          %% If the lookup fails but we have a stale cached result, keep it.
+          case CachedIPAddrs of
+            undefined -> IPAddrs = undefined,
+                         RefreshTS = ?TTL_TO_TS(FailureTTL, Now);
+            _         -> IPAddrs = CachedIPAddrs,
+                         RefreshTS = ?TTL_TO_TS(?MIN_CACHE_SECONDS, Now)
+          end,
+          DNSType = ?DNS_STATIC,
+          Stats = count_failure(CachedStats);
+        SuccessLookup ->
+          {IPAddrs, DNSType} = merge_addrs(SuccessLookup, CachedIPAddrs, Now),
+          RefreshTS = min_ttl_ts(IPAddrs, Now),
+          Stats = count_success(CachedStats, Now)
+      end,
       ets_insert_bounded(?MODULE, #dns_rec{hostname = Host, ip_addrs = IPAddrs,
                                            expiration_time = RefreshTS,
                                            stats = Stats}),
+      DNSType =:= ?DNS_DYNAMIC andalso
+        spawn(fun () -> update_dynamic(Host) end),
       choose_addr(IPAddrs)
   end.
 
@@ -153,24 +169,24 @@ lookup_uncached (Host) ->
   end.
 
 
--spec merge_addrs({list(ip_entry()), ttl()}, ip_addrs() | 'undefined', native_time()) -> {CachedOut::ip_addrs(), RefreshTS::native_time()}.
+-spec merge_addrs({list(ip_entry()), ttl()}, ip_addrs() | 'undefined', native_time()) -> {CachedOut::ip_addrs(), dns_type()}.
 %% Merges a set of new DNS entries in `AddrsIn' into the cached DNS entries in
 %% `CachedIn' and drops expired entries to produce `CachedOut'.
 merge_addrs ({AddrsIn, _TTLIn}, CachedIn, Now) ->
   AddrsInMap = maps:from_list(AddrsIn),
-  {AddrsOutMap, TTLType} =
+  {AddrsOutMap, DNSType} =
     case CachedIn of
       undefined ->
         %% There are no cached addresses because this is the first call.  Use
         %% a short TTL.
-        {AddrsInMap, short_ttl};
+        {AddrsInMap, ?DNS_DYNAMIC};
       _ ->
         %% Copy unexpired entries from the old cache into the new cache.  If
         %% we have any carryovers, that's an indication that the DNS lookup is
         %% giving us a subset of a larger set of IP addresses; in that case,
         %% use a short TTL.
         lists:foldl(
-          fun ({CachedAddr, CachedTS}, Acc={AddrsAcc, TTLTypeAcc}) ->
+          fun ({CachedAddr, CachedTS}, Acc={AddrsAcc, DNSTypeAcc}) ->
               case maps:get(CachedAddr, AddrsAcc, undefined) of
                 undefined ->
                   %% The cached entry was not found in the new address list.
@@ -178,42 +194,75 @@ merge_addrs ({AddrsIn, _TTLIn}, CachedIn, Now) ->
                   %% may be 'infinity', which compares greater than any
                   %% integer.
                   case CachedTS > Now of
-                    true        -> {maps:put(CachedAddr, CachedTS, AddrsAcc), short_ttl};
-                    false       -> {AddrsAcc, short_ttl}
+                    true        -> {maps:put(CachedAddr, CachedTS, AddrsAcc), ?DNS_DYNAMIC};
+                    false       -> {AddrsAcc, ?DNS_DYNAMIC}
                   end;
                 NewTS ->
-                  %% The cached entry was found in the new address list.  If
-                  %% use the greater of the cached TTL and the new TTL for the
+                  %% The cached entry was found in the new address list.  Use
+                  %% the greater of the cached TTL and the new TTL for the
                   %% entry's TTL.
                   case CachedTS > NewTS of
-                    true        -> {maps:put(CachedAddr, CachedTS, AddrsAcc), TTLTypeAcc};
+                    true        -> {maps:put(CachedAddr, CachedTS, AddrsAcc), DNSTypeAcc};
                     false       -> Acc
                   end
               end
-          end, {AddrsInMap, normal_ttl}, tuple_to_list(CachedIn))
+          end, {AddrsInMap, ?DNS_STATIC}, tuple_to_list(CachedIn))
     end,
   AddrsOut = maps:to_list(AddrsOutMap),
 
-  %% Heuristic to decide whether to us a short or long min TTL.  We use a
-  %% short TTL if we think that DNS is giving us one out of a larger set of IP
-  %% addresses for each lookup.  We use a long TTL if we think that the DNS
-  %% server is giving us all of the IP addresses.  We use less than the DNS
-  %% TTL in the long TTL case because we want to collect IPs from multiple DNS
-  %% lookups.
-  TSOut =
-    case TTLType of
-      short_ttl  -> ?TTL_TO_TS(?DYNAMIC_REQUERY_SECONDS, Now);
-      normal_ttl -> max(min_ttl_ts(AddrsOut, ?TTL_TO_TS(?MAX_CACHE_SECONDS, Now)),
-                        ?TTL_TO_TS(?DYNAMIC_REQUERY_SECONDS, Now))
-    end,
+  {list_to_tuple(AddrsOut), DNSType}.
 
-  {list_to_tuple(AddrsOut), TSOut}.
+-spec min_ttl_ts(ip_addrs(), integer()) -> integer().
+min_ttl_ts (IPAddrs, Now) ->
+  AddrsList = tuple_to_list(IPAddrs),
+  max(min_ttl_ts_helper(AddrsList, ?TTL_TO_TS(?MAX_CACHE_SECONDS, Now)),
+      ?TTL_TO_TS(?DYNAMIC_REQUERY_SECONDS, Now)).
+
+-spec min_ttl_ts_helper(list(ip_entry()), integer()) -> integer().
+min_ttl_ts_helper ([ {_, infinity} | Rest ], MinTTL) -> min_ttl_ts_helper(Rest, MinTTL);
+min_ttl_ts_helper ([ {_, TTL} | Rest ], MinTTL) -> min_ttl_ts_helper(Rest, min(TTL, MinTTL));
+min_ttl_ts_helper ([], MinTTL) -> MinTTL.
 
 
--spec min_ttl_ts(list(ip_entry()), integer()) -> integer().
-min_ttl_ts ([ {_, infinity} | Rest ], MinTTL) -> min_ttl_ts(Rest, MinTTL);
-min_ttl_ts ([ {_, TTL} | Rest ], MinTTL) -> min_ttl_ts(Rest, min(TTL, MinTTL));
-min_ttl_ts ([], MinTTL) -> MinTTL.
+update_dynamic (Host) ->
+  Now = lhttpc_dns:monotonic_time(),
+  case ets:lookup(?MODULE, Host) of
+    [] ->
+      CachedIPAddrs = undefined,
+      CachedDynamicCount = 1,
+      CachedStats = #dns_stats{};
+    [ #dns_rec{ip_addrs=CachedIPAddrs, dynamic_count=CachedDynamicCount, stats=CachedStats} ] ->
+      ok
+  end,
+  {IPAddrs, DNSType, Stats} =
+    lists:foldl(
+      fun (_I, {AddrsAcc, DNSTypeAcc, StatsAcc}) ->
+          case lhttpc_dns:lookup_uncached(Host) of
+            {undefined, _} -> {AddrsAcc, DNSTypeAcc, count_failure(StatsAcc)};
+            SuccessLookup  -> {AddrsX, DNSTypeX} = merge_addrs(SuccessLookup, AddrsAcc, Now),
+                              {AddrsX, min(DNSTypeX, DNSTypeAcc), count_success(StatsAcc, Now)}
+          end
+      end, {CachedIPAddrs, ?DNS_STATIC, CachedStats}, lists:seq(1, ?DYNAMIC_REQUERY_COUNT)),
+  DynamicCountOut = if DNSType =:= ?DNS_DYNAMIC -> ?DYNAMIC_REQUERY_COUNT; true -> max(CachedDynamicCount - 1, 0) end,
+  RefreshTS = min_ttl_ts(IPAddrs, Now),
+  ets_insert_bounded(?MODULE, #dns_rec{hostname = Host, ip_addrs = IPAddrs,
+                                       expiration_time = RefreshTS,
+                                       dynamic_count = DynamicCountOut,
+                                       dynamic_pid = case DynamicCountOut of 0 -> undefined; _ -> self() end,
+                                       stats = Stats}),
+  DynamicCountOut > 0 andalso
+    begin
+      receive expire_timer -> ok after ?TS_TO_TTL(RefreshTS, Now) * 1000 -> ok end,
+      update_dynamic(Host)
+    end.
+
+
+%% Prematurely end the update_dynamic wait.  This is used by the tests.
+expire_dynamic (Host) ->
+  case ets:lookup(?MODULE, Host) of
+    [ #dns_rec{dynamic_pid=Pid} ] -> Pid ! expire_timer;
+    _                             -> ok
+  end.
 
 
 -spec ets_insert_bounded(Tab::ets:tab(), Entry::term()) -> 'true'.
@@ -242,12 +291,13 @@ print_cache (Hostname) ->
     _                  -> ok
   end.
 
-print_cache_entry (#dns_rec{hostname=Hostname, ip_addrs=IPAddrs, expiration_time=ExpirationTime, stats={S, F, FS, STS}}, Now) ->
+print_cache_entry (#dns_rec{hostname=Hostname, ip_addrs=IPAddrs, expiration_time=ExpirationTime, stats=Stats}, Now) ->
   IPAddrsIOList = [ io_lib:format("  ~s ~p\n", [ inet:ntoa(IPAddr), ?TS_TO_TTL(TS, Now) ]) || {IPAddr, TS} <- tuple_to_list(IPAddrs) ],
   io:format("~s ttl: ~p sec (~p)\n~s  successes: ~p; failures: ~p; failures_since_last_success: ~p; success_time: ~p sec_ago\n",
             [ Hostname, ?TS_TO_TTL(ExpirationTime, Now), ExpirationTime - Now
             , IPAddrsIOList
-            , S, F, FS, ?TS_TO_TTL(Now, STS)
+            , Stats#dns_stats.success_count, Stats#dns_stats.failure_count, Stats#dns_stats.failures_since_last_success_count
+            , ?TS_TO_TTL(Now, Stats#dns_stats.success_time)
             ]).
 
 
@@ -264,17 +314,17 @@ print_tcpdump_hosts (Hostname) ->
 
 
 -spec count_success(Stats::dns_stats()|'undefined', Now::integer()) -> dns_stats().
-count_success({SuccessCount, FailureCount, _FailuresSinceLastSuccessCount, _LastSuccessTime}, Now) ->
-  {SuccessCount + 1, FailureCount, 0, Now};
-count_success(undefined, Now) ->
-  {1, 0, 0, Now}.
+count_success (Stats=#dns_stats{success_count=SuccessCount}, Now) ->
+  Stats#dns_stats{success_count = SuccessCount + 1, failures_since_last_success_count = 0, success_time = Now};
+count_success (undefined, Now) ->
+  #dns_stats{success_count = 1, success_time = Now}.
 
 
 -spec count_failure(Stats::dns_stats()|'undefined') -> dns_stats().
-count_failure({SuccessCount, FailureCount, FailuresSinceLastSuccessCount, LastSuccessTime}) ->
-  {SuccessCount, FailureCount + 1, FailuresSinceLastSuccessCount + 1, LastSuccessTime};
-count_failure(undefined) ->
-  {0, 1, 1, undefined}.
+count_failure (Stats=#dns_stats{failure_count=FailureCount, failures_since_last_success_count=FailuresSinceLastSuccessCount}) ->
+  Stats#dns_stats{failure_count = FailureCount + 1, failures_since_last_success_count = FailuresSinceLastSuccessCount + 1};
+count_failure (undefined) ->
+  #dns_stats{failure_count = 1, failures_since_last_success_count = 1}.
 
 
 create_table() ->
@@ -298,12 +348,12 @@ check_table () ->
 
 min_ttl_test () ->
   Addr = {0, 1, 2, 3},
-  ?assertEqual(100, min_ttl_ts([], 100)),
-  ?assertEqual(100, min_ttl_ts([ {Addr, 101}, {Addr, 102} ], 100)),
-  ?assertEqual(99, min_ttl_ts([ {Addr, 99}, {Addr, 101} ], 100)),
-  ?assertEqual(99, min_ttl_ts([ {Addr, 101}, {Addr, 99} ], 100)),
-  ?assertEqual(99, min_ttl_ts([ {Addr, infinity}, {Addr, 99} ], 100)),
-  ?assertEqual(100, min_ttl_ts([ {Addr, 101}, {Addr, infinity} ], 100)),
+  ?assertEqual(100, min_ttl_ts_helper([], 100)),
+  ?assertEqual(100, min_ttl_ts_helper([ {Addr, 101}, {Addr, 102} ], 100)),
+  ?assertEqual(99, min_ttl_ts_helper([ {Addr, 99}, {Addr, 101} ], 100)),
+  ?assertEqual(99, min_ttl_ts_helper([ {Addr, 101}, {Addr, 99} ], 100)),
+  ?assertEqual(99, min_ttl_ts_helper([ {Addr, infinity}, {Addr, 99} ], 100)),
+  ?assertEqual(100, min_ttl_ts_helper([ {Addr, 101}, {Addr, infinity} ], 100)),
   ok.
 
 %% Make a dummy ip_entry.
@@ -311,10 +361,9 @@ min_ttl_test () ->
 
 merge_addrs_test () ->
   TS0 = 0,
-  DynamicTS0 = ?TTL_TO_TS(?DYNAMIC_REQUERY_SECONDS, TS0),
 
   ?assertMatch(
-     {{?A(1, 300)}, DynamicTS0},
+     {{?A(1, 300)}, ?DNS_DYNAMIC},
      merge_addrs({[ ?A(1, 300) ], 1}, undefined, TS0)),
   %% Keep the max TTL.
   ?assertMatch(
@@ -328,15 +377,14 @@ merge_addrs_test () ->
      merge_addrs({[ ?A(1, 300) ], 1}, {?A(1, 400)}, TS0)),
 
   TS200 = 200,
-  DynamicTS200 = ?TTL_TO_TS(?DYNAMIC_REQUERY_SECONDS, TS200),
 
   %% Short vs long TTL, don't keep cache.
   ?assertMatch(
-     {{?A(2, 300)}, DynamicTS200},
+     {{?A(2, 300)}, ?DNS_DYNAMIC},
      merge_addrs({[ ?A(2, 300) ], 1}, {?A(1, 100)}, TS200)),
   %% Short vs long TTL, keep cache.
   ?assertMatch(
-     {{?A(1, 250), ?A(2, 300)}, DynamicTS200},
+     {{?A(1, 250), ?A(2, 300)}, ?DNS_DYNAMIC},
      merge_addrs({[ ?A(2, 300) ], 1}, {?A(1, 250)}, TS200)),
   ok.
 
--- test/lhttpc_dns_tests.erl
+++ test/lhttpc_dns_tests.erl
@@ -38,13 +38,18 @@ test_lookup_uncached () ->
 -define(SINGLE, {4, 0, 0, 0}).
 -define(DYNAMIC(N), {5, 0, 0, N rem 8}).
 
+-define(DYNAMIC_REQUERY_COUNT, 12).
+
+test_put (Key, Value) -> ets:insert(lhttpc_dns, {norecord, Key, Value}).
+test_get (Key) -> ets:lookup_element(lhttpc_dns, Key, 3).
+
 test_lookup () ->
   lhttpc_dns:reset_table(),
-  erlang:put(dns_lookup, 0),
-  erlang:put(dns_random, 0),
-  erlang:put(dns_ts, 0),
+  test_put(dns_lookup, 0),
+  test_put(dns_random, 0),
+  test_put(dns_ts, 0),
 
-  MonotonicTimeFun = fun () -> erlang:convert_time_unit(erlang:get(dns_ts), seconds, native) end,
+  MonotonicTimeFun = fun () -> erlang:convert_time_unit(test_get(dns_ts), seconds, native) end,
   LookupFun = fun (IPs, TTL) ->
                TTLTS = MonotonicTimeFun() + erlang:convert_time_unit(TTL, second, native),
                {[ {IP, TTLTS} || IP <- IPs ], TTL}
@@ -53,17 +58,17 @@ test_lookup () ->
   meck:new(lhttpc_dns, [ passthrough ]),
   meck:expect(lhttpc_dns, monotonic_time, MonotonicTimeFun),
   meck:expect(lhttpc_dns, lookup_uncached,
-              fun(Host) ->
+              fun (Host) ->
                   %% io:format(standard_error, "host: ~p~n", [ Host ]),
-                  case erlang:get(dns_lookup) of
-                    dns_lookup_fail   -> erlang:put(dns_lookup, 0),
+                  case test_get(dns_lookup) of
+                    dns_lookup_fail   -> test_put(dns_lookup, 0),
                                          {undefined, 1};
                     N ->
                       case Host of
                         "localhost"   -> LookupFun([ ?LOCALHOST ], 3600);
                         "single.com"  -> LookupFun([ ?SINGLE ], 1800);
                         "multi.com"   -> LookupFun([ ?MULTI1, ?MULTI2 ], 300);
-                        "dynamic.com" -> erlang:put(dns_lookup, N + 1),
+                        "dynamic.com" -> test_put(dns_lookup, N + 1),
                                          LookupFun([ ?DYNAMIC(N) ], 60);
                         _             -> {undefined, 1}
                       end
@@ -71,7 +76,7 @@ test_lookup () ->
               end),
 
   meck:new(rand, [ passthrough, unstick ]),
-  meck:expect(rand, uniform, fun (N) -> I = erlang:get(dns_random), erlang:put(dns_random, I + 1), I rem N + 1 end),
+  meck:expect(rand, uniform, fun (N) -> I = test_get(dns_random), test_put(dns_random, I + 1), I rem N + 1 end),
 
   %% Verify that our function has been successfully mecked.
   ?assertMatch({[ {?LOCALHOST, _} ], 3600}, lhttpc_dns:lookup_uncached("localhost")),
@@ -79,88 +84,91 @@ test_lookup () ->
   %% Verify that IP address caching is working.
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
   lhttpc_dns:print_cache("single.com"),
-  ?assertEqual(1, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 60),
+  ?assertEqual(1 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 60),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(2, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  ?assertEqual(1 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
   lhttpc_dns:print_cache("single.com"),
-  erlang:put(dns_ts, 70),
-  ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  lhttpc_dns:print_cache("single.com"),
-  ?assertEqual(2, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 2400),
+  test_put(dns_ts, 70),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
   lhttpc_dns:print_cache("single.com"),
-  ?assertEqual(3, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  ?assertEqual(1 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 2400),
+  ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
+  lhttpc_dns:print_cache("single.com"),
+  ?assertEqual(2 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
 
   %% When a hostname has multiple IP addresses, a random one is returned.
-  erlang:put(dns_ts, 3600),
-  erlang:put(dns_random, 0),
+  test_put(dns_ts, 3600),
+  test_put(dns_random, 0),
   ?assertEqual(?MULTI1, lhttpc_dns:lookup("multi.com")),
   ?assertEqual(?MULTI2, lhttpc_dns:lookup("multi.com")),
-  ?assertEqual(1, meck:num_calls(lhttpc_dns, lookup_uncached, [ "multi.com" ])),
-  erlang:put(dns_ts, 3630),
+  timer:sleep(10), %% Wait to give the dynamic lookups time to happen.
+  ?assertEqual(1 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "multi.com" ])),
+  test_put(dns_ts, 3630),
   ?assertEqual(?MULTI1, lhttpc_dns:lookup("multi.com")),
-  ?assertEqual(2, meck:num_calls(lhttpc_dns, lookup_uncached, [ "multi.com" ])),
-  erlang:put(dns_ts, 3720),
+  ?assertEqual(1 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "multi.com" ])),
+  test_put(dns_ts, 3920),
   ?assertEqual(?MULTI2, lhttpc_dns:lookup("multi.com")),
-  ?assertEqual(2, meck:num_calls(lhttpc_dns, lookup_uncached, [ "multi.com" ])),
+  ?assertEqual(2 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "multi.com" ])),
 
   %% When a DNS lookup for a cached hostname fails, the old value is cached
   %% for an additional MIN_CACHE_SECONDS.
-  erlang:put(dns_ts, 7200),
-  erlang:put(dns_lookup, dns_lookup_fail),
+  test_put(dns_ts, 7200),
+  test_put(dns_lookup, dns_lookup_fail),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(4, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 7201),
+  ?assertEqual(3 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 7201),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(4, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 7600),
+  ?assertEqual(3 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 7600),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(5, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 7601),
+  ?assertEqual(4 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 7601),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(5, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 8000),
+  ?assertEqual(4 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 8000),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(5, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_ts, 10800),
+  ?assertEqual(4 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_ts, 10800),
   ?assertEqual(?SINGLE, lhttpc_dns:lookup("single.com")),
-  ?assertEqual(6, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
-  erlang:put(dns_lookup, 0),
+  ?assertEqual(5 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "single.com" ])),
+  test_put(dns_lookup, 0),
 
   %% Error lookups are only cached for ERROR_CACHE_SECONDS.
   ?assertEqual(undefined, lhttpc_dns:lookup("noaddr.com")),
   ?assertEqual(undefined, lhttpc_dns:lookup("noaddr.com")),
   ?assertEqual(1, meck:num_calls(lhttpc_dns, lookup_uncached, [ "noaddr.com" ])),
-  erlang:put(dns_ts, 10802),
+  test_put(dns_ts, 10802),
   ?assertEqual(undefined, lhttpc_dns:lookup("noaddr.com")),
   ?assertEqual(2, meck:num_calls(lhttpc_dns, lookup_uncached, [ "noaddr.com" ])),
 
   %% Dynamic lookups.
-  %% These tests are unfortunately tied to the implementation details more
-  %% than I would like and thus are much more brittle than I would like.
-  erlang:put(dns_lookup, 0),
-  erlang:put(dns_ts, 12000),
+  %% These tests are unfortunately tied to the implementation details and are
+  %% thus much more brittle than I would like.
+  test_put(dns_lookup, 0),
+  test_put(dns_random, 0),
+  test_put(dns_ts, 12000),
   ?assertEqual({5, 0, 0, 0}, lhttpc_dns:lookup("dynamic.com")),
-  %% If the next lookup is less than the TTL lhttpc_dns doesn't realize that
-  %% the address is dynamic and so it doesn't do another query.
-  erlang:put(dns_ts, 12001), % printrec("dynamic.com"),
+  timer:sleep(10), %% Wait to give the dynamic lookups time to happen.
+  ?assertEqual(1 + ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "dynamic.com" ])),
+  test_put(dns_random, 0),
+  test_put(dns_ts, 12001), % printrec("dynamic.com"),
   ?assertEqual({5, 0, 0, 0}, lhttpc_dns:lookup("dynamic.com")),
+  ?assertEqual({5, 0, 0, 1}, lhttpc_dns:lookup("dynamic.com")),
+  ?assertEqual({5, 0, 0, 2}, lhttpc_dns:lookup("dynamic.com")),
   %% This is the second lookup after seeing the hostname for the first time,
   %% so there is a short TTL.
-  erlang:put(dns_ts, 12005), % printrec("dynamic.com"),
-  ?assertEqual([ {5, 0, 0, 0}, {5, 0, 0, 1} ], repeat_lookup("dynamic.com", 8)),
-  erlang:put(dns_ts, 12006),
-  ?assertEqual([ {5, 0, 0, 0}, {5, 0, 0, 1} ], repeat_lookup("dynamic.com", 8)),
-  erlang:put(dns_ts, 12010), % printrec("dynamic.com"),
-  ?assertEqual([ {5, 0, 0, 0}, {5, 0, 0, 1}, {5, 0, 0, 2} ], repeat_lookup("dynamic.com", 8)),
-  erlang:put(dns_ts, 12055), % printrec("dynamic.com"),
-  ?assertEqual([ {5, 0, 0, 0}, {5, 0, 0, 1}, {5, 0, 0, 2}, {5, 0, 0, 3} ], repeat_lookup("dynamic.com", 8)),
-  erlang:put(dns_ts, 12060), % printrec("dynamic.com"),
-  ?assertEqual([ {5, 0, 0, 1}, {5, 0, 0, 2}, {5, 0, 0, 3}, {5, 0, 0, 4} ], repeat_lookup("dynamic.com", 8)),
-  erlang:put(dns_ts, 12070), % printrec("dynamic.com"),
-  ?assertEqual([ {5, 0, 0, 3}, {5, 0, 0, 4}, {5, 0, 0, 5} ], repeat_lookup("dynamic.com", 8)),
+  test_put(dns_ts, 12005), % printrec("dynamic.com"),
+  ?assertEqual([ {5, 0, 0, 0}, {5, 0, 0, 1}, {5, 0, 0, 2}, {5, 0, 0, 3}, {5, 0, 0, 4}, {5, 0, 0, 5}, {5, 0, 0, 6} , {5, 0, 0, 7} ],
+               repeat_lookup("dynamic.com", 8)),
+
+  test_put(dns_ts, 12065), % printrec("dynamic.com"),
+  lhttpc_dns:expire_dynamic("dynamic.com"),
+  timer:sleep(10),
+  ?assertEqual(1 + 2 * ?DYNAMIC_REQUERY_COUNT, meck:num_calls(lhttpc_dns, lookup_uncached, [ "dynamic.com" ])),
+  ?assertEqual([ {5, 0, 0, 0}, {5, 0, 0, 1}, {5, 0, 0, 2}, {5, 0, 0, 3}, {5, 0, 0, 4}, {5, 0, 0, 5}, {5, 0, 0, 6} , {5, 0, 0, 7} ],
+               repeat_lookup("dynamic.com", 8)),
 
   meck:unload(rand),
   meck:unload(lhttpc_dns).
@@ -176,4 +184,4 @@ repeat (N, Fun, Acc) ->
 
 printrec (Host) ->
   lhttpc_dns:lookup(Host),
-  io:format(user, "dynamic ~p\n  ~p\n", [ erlang:get(dns_ts), ets:lookup(lhttpc_dns, Host) ]).
+  io:format("dynamic ~p\n  ~p\n", [ test_get(dns_ts), ets:lookup(lhttpc_dns, Host) ]).
